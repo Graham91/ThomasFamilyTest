3a90d7cad2bf647d7e3d7bcad798b8e59ce6036e
grambo <gramham@live.com>
2021-01-12 15:39:06 -0700

    working on adding react structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav/";
+import BooksSearch from "./components/BooksSearch/";
+import FavoriteBooks from "./components/Favoritebooks/";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Nav />
+          <Route exact path="/">
+            <BooksSearch />
+          </Route>
+          <Route exact path="/profile">
+            <FavoriteBooks />
+          </Route>
+          <Route exact path="/authProfile"></Route>
+          <Route exact path="/test"></Route>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
