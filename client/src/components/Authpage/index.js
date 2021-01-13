39e08c788276bb6700e4d91d18bf191fbd9f252c
grambo <gramham@live.com>
2021-01-13 16:26:06 -0700

    add more structure

--- client/src/components/Authpage/index.js
+++ client/src/components/Authpage/index.js
@@ -5,8 +5,17 @@ import "./Authpage.css";
 function AuthPage(props) {
   return (
     <div>
-      <Link to="/">back</Link>
-      <p>AuthPage</p>
+      <div className="managementHome">
+        <Link to="/" className="backbutton">
+          back
+        </Link>
+        <div className="optionsBox">
+          <div className="options">Manage Users</div>
+          <div className="options">Add Test</div>
+          <div className="options">View saved Tested</div>
+          <div className="options">Edit tests</div>
+        </div>
+      </div>
     </div>
   );
 }
